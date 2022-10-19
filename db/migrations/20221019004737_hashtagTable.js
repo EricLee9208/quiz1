154cece36b21cdee11a85457606ebab3e957a145
eric-lee <eric9208@hotmail.com>
2022-10-18 20:05:50 -0700

    finished

---
+++ db/migrations/20221019004737_hashtagTable.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("hashtag", (table) => {
+    table.bigIncrements("id");
+    table.text("tag");
+    table.integer("count");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("hashtag");
+};
--- routes/cluckRouter.js
+++ routes/cluckRouter.js
@@ -19,14 +19,55 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/new", (req, res) => {
+  const content = req.body.content.toString();
+  let hashtagObj = {};
+
+  if (content.indexOf("#") > -1) {
+    let contentArr = content.split(" ");
+    for (const word of contentArr) {
+      if (word.indexOf("#") > -1) {
+        if (hashtagObj[word]) {
+          hashtagObj[word] += 1;
+        } else {
+          hashtagObj[word] = 1;
+        }
+      }
+    }
+  }
+  //   console.log(hashtagObj);
+
   knex("cluckr")
     .insert({
       username: res.locals.username,
       content: req.body.content,
       imageUrl: req.body.imageUrl,
     })
-    .returning("*")
+    // .then(() => {
+    //   for (const key in hashtagObj) {
+    //     knex
+    //       .select("count")
+    //       .from("hashtag")
+    //       .where("tag", key)
+    //       .then((counts) => {
+    //         // then inside for loop 11111
+    //         console.log(counts);
+    //         console.log(key);
+    //         console.log(hashtagObj[key]);
+    //         if (counts.length === 0) {
+    //           knex("hashtag").insert({
+    //             tag: key,
+    //             count: hashtagObj[key],
+    //           }); // closing insert
+    //         } // closing if
+    //         else {
+    //           knex("hashtag")
+    //             .where("tag", key)
+    //             .increment("count", hashtagObj[key]); // closing update
+    //         } // closing else
+    //       }); // then inside for loop 1111
+    //   } // closing for loop
+    // }) // closing then
     .then(() => res.redirect("/cluck"));
-});
+}); // closing post function
 
 module.exports = router;
